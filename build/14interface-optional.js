c444155be43bb725221ef4b7f8ce086bb686b51c
muchfahmiarif <fahmiarif96@gmail.com>
2023-02-11 12:17:45 +0700

    interface-optional

---
+++ build/14interface-optional.js
@@ -0,0 +1,5 @@
+"use strict";
+let teacher = {
+    name: "John",
+    age: 30,
+};
---
+++ src/14interface-optional.ts
@@ -0,0 +1,10 @@
+interface Teacher {
+  name: string;
+  age: number;
+  phone?: string;
+}
+
+let teacher: Teacher = {
+  name: "John",
+  age: 30,
+};
